f59c4d67fd91ba3d20d4c365025416fa9ba32529
Maria Elena Raspo <126521941+Mariaprofe@users.noreply.github.com>
2025-09-07 22:35:13 -0300

    a

---
+++ formulario5.js
@@ -0,0 +1,22 @@
+// Los números de socios válidos van desde 1000 a 5000 (inclusive)
+
+var presultado = document.getElementById("resultado");
+var presultado2 = document.getElementById("resultado2");
+function validar(){
+    var resp = document.getElementById("resp").value;
+
+
+/*&& resp <=5000*/
+if(resp!=1){
+        presultado.innerHTML = '<strong class="exito"><i class="fa-solid fa-circle-check"></i>  Es Correcta su respesta eso indica entendimiento del tema.</strong>';
+    } else{
+        presultado.innerHTML = '<strong class="error"><i class="fa-solid fa-triangle-exclamation"></i> Incorrecto  vuelve a leer</strong>'
+    }
+var resp1 = document.getElementById("resp1").value;
+if(resp1==1){
+         presultado2.innerHTML = '<strong class="exito1"><i class="fa-solid fa-circle-check"></i>  Es Correcta su respesta eso indica entendimiento del tema.</strong>';
+     } else{
+         presultado2.innerHTML = '<strong class="error1"><i class="fa-solid fa-triangle-exclamation"></i> Incorrecto  vuelve a leer</strong>'
+     }
+
+ }
